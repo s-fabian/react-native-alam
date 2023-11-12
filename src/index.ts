2353e17229ebc3a3f38c42691eff269b2e1cb009
Fabian <spiele2014fs@gmail.com>
2023-11-12 18:13:44 +0100

    Add responsive varients
    Add the following responsive varients:
    
    - `xs-example`
    - `sm-example`
    - `md-example`
    - `lg-example`
    - `xl-example`
    - `2xl-example`
    - `min-xs-example`
    - `min-sm-example`
    - `min-md-example`
    - `min-lg-example`
    - `min-xl-example`
    - `min-2xl-example`
    - `example`

--- src/index.ts
+++ src/index.ts
@@ -21,9 +21,23 @@ export type DefaultAlam = typeof defaultAlam;
 export type ExtendedAlam = DefaultAlam &
   Record<string, (arg0: any, style: Style, colors: Colors) => Style>;
 
-export type TailwindArgs<A extends ExtendedAlam = DefaultAlam> = {
+export type TailwindArgs<A extends ExtendedAlam = DefaultAlam> = ApplyResponsive<{
   [key in keyof A]?: Parameters<A[key]>[0];
+}>;
+
+type StringPropertyNames<T> = {
+  [K in keyof T]: K extends string ? K : never
+}[keyof T];
+
+type ApplyResponsive<T> = {
+  [K in StringPropertyNames<T>]: T[K]
+} & {
+  [K in StringPropertyNames<T> as Responsive<K>]: T[K]
 };
+  
+type ResponsiveMin<V extends string> = `xs-${V}` | `sm-${V}` | `md-${V}` | `lg-${V}` | `xl-${V}` | `2xl-${V}`;
+type ResponsiveMinMax<V extends string> = ResponsiveMin<V> | `min-${ResponsiveMin<V>}`;
+type Responsive<V extends string> = ResponsiveMinMax<V> | V
 
 type Banned<_> = {
   [key: string]: any;
