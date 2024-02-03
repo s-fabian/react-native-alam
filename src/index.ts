601257fe0797e9a834fa8a0b199723f5de71a779
Fabian <spiele2014fs@gmail.com>
2024-02-03 19:05:42 +0100

    Add default export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { createAlam } from './components';
+export { createAlam, createAlam as default } from './components';
 export { useTheme, ThemeProvider } from './theme';
 export type { Colors, Style, AlamDef, GetProps, Finish } from './types';
 export type { DefaultProps } from './props';
